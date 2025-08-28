40c46106bc60637907299832b82aee4b422f9c37
KenfordMustapha1 <163841892+KenfordMustapha1@users.noreply.github.com>
2025-08-28 08:46:45 +0800

    Update class_management_server.js

--- class_management_server.js
+++ class_management_server.js
@@ -5,64 +5,44 @@ const PORT = 3000;
 app.use(express.json());
 
 let users = [
-    {id: 1, lastName: `Mustapha`, firstName:`Ken Ford`, section:`BSIT4A`, status:`p`},
-    {id: 1, lastName: `Mustapha`, firstName:`Ken Ford`, section:`BSIT4A`, status:`a`},
+    { id: 1, lastName: `Mustapha`, firstName: `Ken Ford`, section: `BSIT 4A`, status: `p` },
+    { id: 2, lastName: `Mustapha`, firstName: `Ken Ford`, section: `BSIT 4A`, status: `a` }
 ];
 
-app.post(`/users`, (req, res)=>{
+app.post(`/users`, (req, res) => {
+    const { lastName, firstName, section, status } = req.body;
 
-const {lastName, firstName, section, status} = req.body;
+    const userIndex = users.findIndex(user => user.firstName === firstName && user.lastName === lastName);
 
-const userIndex = users.findIndex(user=> user.firstName === firstName && user.lastName === lastName);
+    if (userIndex !== -1) {
+        users[userIndex].status = status;
+        console.log(`Updated attendance for ${lastName} ${firstName} to ${status}`);
+        res.status(200).json({ message: `Attendance for ${lastName} ${firstName} has been updated to ${status}.` });
+    } else {
+        const newUser = {
+            id: users.length + 1,
+            lastName,
+            firstName,
+            section,
+            status
+        };
 
-if(userIndex !== -1){
-
-user[userIndex].status = status;
-console.log(`Updated attendance for ${lastName} ${firstName} tp: ${status}`);
-res.status(200).json({ message: `Attendance for ${lastName} ${firstName} has been updated to ${status}.`});
-
-}else{
-
-
-    
-};
-
-const newUser = {
-    id: users.length + 1,
-    lastName,
-    firstName,
-    section,
-    status,
-};
-
-
-
-users.push(newUser);
-console.log(`New user added: ${lastName} ${firstName} with status: ${status}`);
-
-res.status(201).json({message: `New student ${lastName} ${firstName} has been added with status ${status}`});
-
+        users.push(newUser);
+        console.log(`New user added: ${lastName} ${firstName} with status: ${status}`);
+        res.status(201).json({ message: `New student ${lastName} ${firstName} has been added with status: ${status}` });
+    }
 });
 
-
-app.get(`/users`, (req, res)=>{
-    res.status(200).json(users); 
-
-    
-app.get(`/`, (req, res)=>{
-    res.send(`Server is running!`)
+app.get(`/users`, (req, res) => {
+    res.status(200).json(users);
 });
 
-
-app.listen(PORT, () =>{
-    console.log(`Server is running at https://localhost:${PORT}`);
+app.get(`/`, (req, res) => {
+    res.send(`Server is up and running!`);
 });
 
-
-
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 module.exports = app;
-
-
-
